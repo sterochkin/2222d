62c4b8d82591dab0a2bdfe1c8e20730342f908d2
Maks <brezgin1996@mail.ru>
2022-08-24 23:31:50 +0300

    ProjectX

--- telegram.php
+++ telegram.php
@@ -18,9 +18,7 @@ foreach($arr as $key => $value){
 $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
 
 if ($sendToTelegram){
-  <<<HTML
-		<script src="js/modal.js"></script>
-HTML;
+
 }else {
   echo "Error";
 }
